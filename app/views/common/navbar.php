81dde255b39f66c545efe53661af5d3e9939a254
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-01-08 12:11:28 +0530

    Delete app directory

--- app/views/common/navbar.php
+++
@@ -1,9 +0,0 @@
-<div class="navbar">
-    <h2>TapToBus</h2>
-
-    <ul>
-        <li><a href="<?php echo URLROOT; ?>/common/index">Home</a></li>
-        <li><a href="<?php echo URLROOT; ?>/common/terms_conditions">Terms & Conditions</a></li>
-        <li><a href="<?php echo URLROOT; ?>/common/contact_us">Contact Us</a></li>
-    </ul>
-</div>
