b7070b8f1cd375f178dfaa76c02227e250e48629
Adheesha <adheeshachamoddesilva@gmail.com>
2023-04-12 19:49:34 +0530

    Partially, implement select_seats part
    Update select_seats view, it's css and js

--- app/views/passenger/select_seats.php
+++ app/views/passenger/select_seats.php
@@ -13,51 +13,95 @@
     <?php require APPROOT . '/views/inc/passenger_navbar.php' ?>
 
     <div class="main">
+        <div class="content">
+            <h1 class="heading">Seat Arrangement</h1>
 
-        <div class="seating-chart">
-            <div class="row">
-                <label for="seat1" class="seat-label">
-                    <input type="checkbox" id="seat1" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat2" class="seat-label">
-                    <input type="checkbox" id="seat2" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat3" class="seat-label">
-                    <input type="checkbox" id="seat3" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat4" class="seat-label">
-                    <input type="checkbox" id="seat4" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-            </div>
-            <div class="row">
-                <label for="seat5" class="seat-label">
-                    <input type="checkbox" id="seat5" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat6" class="seat-label">
-                    <input type="checkbox" id="seat6" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat7" class="seat-label">
-                    <input type="checkbox" id="seat7" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat8" class="seat-label">
-                    <input type="checkbox" id="seat8" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
-                <label for="seat9" class="seat-label">
-                    <input type="checkbox" id="seat9" class="seat" />
-                    <span class="checkmark"></span>
-                </label>
+            <div class="details">
+                <div class="left">
+                    <div class="left-heading">Front</div>
+
+                    <div class="seat-arrangement">
+                        <?php $line = 0; ?>
+
+                        <?php while($line <= 7): ?>
+                            <div class="row">
+                                <span class="col1"><input type="checkbox" name="s<?php echo $line+1; ?>>" id="s<?php echo $line+1; ?>>" class="seat"></span>
+                                <span class="col2"><input type="checkbox" name="s<?php echo $line+2; ?>>" id="s<?php echo $line+2; ?>>" class="seat"></span>
+                                <span class="col3"></span>
+                                <span class="col4"><input type="checkbox" name="s<?php echo $line+3; ?>" id="<?php echo $line+3; ?>s" class="seat"></span>
+                                <span class="col5"><input type="checkbox" name="s<?php echo $line+4; ?>" id="s<?php echo $line+4; ?>" class="seat"></span>
+                            </div>
+
+                            <?php $line++; ?>
+                        <?php endwhile; ?>
+
+                        <div class="row">
+                            <span class="col1"><input type="checkbox" name="s<?php echo $line+1; ?>>" id="s<?php echo $line+1; ?>>" class="seat"></span>
+                            <span class="col2"><input type="checkbox" name="s<?php echo $line+2; ?>>" id="s<?php echo $line+2; ?>>" class="seat"></span>
+                            <span class="col3"><input type="checkbox" name="s<?php echo $line+3; ?>" id="<?php echo $line+3; ?>s" class="seat"></span>
+                            <span class="col4"><input type="checkbox" name="s<?php echo $line+4; ?>" id="<?php echo $line+4; ?>s" class="seat"></span>
+                            <span class="col5"><input type="checkbox" name="s<?php echo $line+5; ?>" id="s<?php echo $line+5; ?>" class="seat"></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right">
+                    <div class="right-top">
+                        <div class="right-top-heading">Summary</div>
+
+                        <div class="right-top-details">
+                            <div class="journey">Galle > Makumbura</div>
+
+                            <div class="row">
+                                <span class="col1">On:</span>
+                                <span class="col2">2023-04-14</span>
+                            </div>
+                            <div class="row">
+                                <span class="col1">At:</span>
+                                <span class="col2">10:30 AM</span>
+                            </div>
+                            <div class="row">
+                                <span class="col1">Passenger count:</span>
+                                <span class="col2">4</span>
+                            </div>
+                            <div class="row">
+                                <span class="col1">Bus No:</span>
+                                <span class="col2">NB-1234</span>
+                            </div>
+                            <div class="row">
+                                <span class="col1">Ticket price:</span>
+                                <span class="col2">LKR 5000.00</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="right-bottom">
+                        <div class="right-bottom-heading">Select Seats</div>
+                        <div class="description">You can select 4 seats</div>
+
+                        <div class="legend">
+                            <span class="col">
+                                <span class="squ"><i class="fa-regular fa-square available"></i></span>
+                                <span class="desc">Available</span>
+                            </span>
+                            <span class="col">
+                                <span class="squ"><i class="fa-solid fa-square not-available"></i></span>
+                                <span class="desc">Not available</span>
+                            </span>
+                            <span class="col">
+                                <span class="squ"><i class="fa-solid fa-square select"></i></span>
+                                <span class="desc">Select</span>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="btn">
+                        <button class="btn-left" onclick="goBack()">Back</button>
+                        <button class="btn-right" onclick="goNext('<?php echo $bus_no ?>', '<?php echo $capacity ?>', '<?php echo $schedule_id ?>', '<?php echo $booked_seats_id ?>', '<?php echo $count ?>')">Pay</button>
+                    </div>
+                </div>
             </div>
         </div>
-
-
     </div>
 
     <script src="<?php echo URLROOT; ?>/js/passenger-js/select-seats-js.js"></script>
