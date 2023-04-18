394b8f5e9e3c68d88a9bee744ba45f88da2c162c
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-04-18 17:24:05 +0530

    Update M_Admin_bus_details.php

--- app/models/M_Admin_bus_details.php
+++ app/models/M_Admin_bus_details.php
@@ -15,5 +15,18 @@ class M_Admin_bus_details {
         return $this->db->resultSet();
 
     }
+
+    public function removebuses(){
+        $this->db->query('SELECT * FROM bus WHERE status = "pending"');
+        return $this->db->resultSet();
+    }
+
+    public function resetbuses($bus_no){
+        $this->db->query('UPDATE bus SET status = "active" WHERE bus_no = :bus_no');
+        $this->db->bind(":bus_no",$bus_no);
+        return $this->db->execute();
+
+    }
+
 }
 ?>
