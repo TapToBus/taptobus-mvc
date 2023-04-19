50fd54a3496e56bfbbc3f8d90842558171990df2
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-04-19 20:59:55 +0530

    update navbar

--- app/views/inc/conductor_navbar.php
+++ app/views/inc/conductor_navbar.php
@@ -36,10 +36,11 @@
     </div>
     
     <a href="<?php echo URLROOT; ?>/conductor_dashboard/view_dashboard"><i class="fa-solid fa-house"></i><span>Dashboard</span></a>
-    <a href="<?php echo URLROOT; ?>/conductor_incomerecords/add_incomerecords"><i class="fa-solid fa-bus"></i><span>Income Records</span></a>
+    <a href="<?php echo URLROOT; ?>/conductor_incomerecords/add_incomerecords"><i class="fa-solid fa-note"></i><span>Income Records</span></a>
     <a href="<?php echo URLROOT; ?>/conductor_schedule/view_schedule"><i class="fa-solid fa-user"></i><span>Schedule</span></a>
     <a href="<?php echo URLROOT; ?>/conductor_leaverequests/add_leaverequests"><i class="fa-solid fa-newspaper"></i><span>Leave Requests</span></a>
     <a href="<?php echo URLROOT; ?>/conductor_notifications/view_notifications"><i class="fa-solid fa-bell"></i><span>Notifications</span></a>
     <a href="#"><i class="fa-solid fa-user"></i><span>Profile</span></a>
     <a href="<?php echo URLROOT; ?>/users/logout"><i class="fa-solid fa-right-from-bracket"></i><span>Logout</span></a>
 </div>
+
