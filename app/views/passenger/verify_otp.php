487d802b7a5cee0f8643d4ad64f84a8fedbed6f4
Adheesha <adheeshachamoddesilva@gmail.com>
2023-04-21 19:24:43 +0530

    Add style to the verify_otp part

--- app/views/passenger/verify_otp.php
+++ app/views/passenger/verify_otp.php
@@ -5,13 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger-style/map-style.css"> -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger-style/verify-otp-style.css">
     <title><?php echo SITENAME; ?></title>
 </head>
 
 <body>
     <div class="box">
         <form action="<?php echo URLROOT; ?>/passenger_register/verify_otp?id=<?php echo $_GET['id'] ?>" method="post">
+            <div class="logo">
+                <img src="<?php echo URLROOT; ?>/img/logo-black.png" alt="Logo" srcset="">
+            </div>
+
+            <div class="note">
+                <span>We sent you an email to the provided email. Please check it.</span>
+            </div>
+
             <div class="otp">
                 <label for="otp">Enter OTP</label> <br>
                 <input type="text" name="otp" placeholder="XXXXXX" value="<?php echo $data['otp']; ?>"> <br>
