ae2ca8029dda92f22f9ce17ac3b88dbb0e591547
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-04-26 16:24:51 +0530

    staff member add form view styles update and model update

--- app/views/admin/add_new_staff_member.php
+++ app/views/admin/add_new_staff_member.php
@@ -16,40 +16,32 @@
 
     <div class="main">
 
-        <div class="content-heading">
-            <h1>ADD NEW STAFF MEMBER</h1>
-            <!-- <hr> -->
-        </div>
-
         <form action="<?php echo URLROOT?>/Admin_add_staff_member/insert_new_staff_member " method="POST"class="add_new_staff_member_form">
-            <!-- <h1 class="add_new_staff_member_title">Add staff member</h1> -->
+            <h1 class="add_new_staff_member_title">ADD NEW STAFF MEMBER</h1>
 
-            <!-- <label for="staff_id" id="staff_id" class="add_new_staff_member_label">Staff Id</label> -->
-            <input type="text" name="staff_id" id="staffid" class="add_new_staff_member_input" placeholder="   Staff Id">
+            <label for="staff_id" id="staff_id" class="add_new_staff_member_label">Staff Id</label>
+            <input type="text" name="staff_id" id="staffid" class="add_new_staff_member_input">
 
-            <!-- <label for="nic" class="add_new_staff_member_label">NIC</label> -->
-            <input type="text" name="nic" id="nic" class="add_new_staff_member_input" placeholder="   NIC">
+            <label for="nic" class="add_new_staff_member_label">NIC</label>
+            <input type="text" name="nic" id="nic" class="add_new_staff_member_input">
 
-            <!-- <label for="first_name" class="add_new_staff_member_label">First name</label> -->
-            <input type="text" id="firstname" name="first_name" class="add_new_staff_member_input" placeholder="   First Name">
+            <label for="first_name" class="add_new_staff_member_label">First name</label>
+            <input type="text" id="firstname" name="first_name" class="add_new_staff_member_input">
 
-            <!-- <label for="last_name" class="add_new_staff_member_label">Last name</label> -->
-            <input type="text" id="lastname" name="last_name" class="add_new_staff_member_input" placeholder="   Last Name">
+            <label for="last_name" class="add_new_staff_member_label">Last name</label>
+            <input type="text" id="lastname" name="last_name" class="add_new_staff_member_input">
 
-            <!-- <label for="dob" class="add_new_staff_member_label">Date Of Birth</label> -->
-            <input type="text" id="dob" name="dob" class="add_new_staff_member_input" placeholder="   Date of Birth">
+            <label for="dob" class="add_new_staff_member_label">Date Of Birth</label>
+            <input type="date" id="dob" name="dob" class="add_new_staff_member_input">
 
-            <!-- <label for="address" class="add_new_staff_member_label">Address</label> -->
-            <!-- <input type="text" id="address" name="address" class="add_new_staff_member_input" placeholder="   Address"> -->
+            <label for="email" class="add_new_staff_member_label">Email</label>
+            <input type="email" id="email" name="email" class="add_new_staff_member_input">
 
-            <!-- <label for="email" class="add_new_staff_member_label">Email</label> -->
-            <input type="text" id="email" name="email" class="add_new_staff_member_input" placeholder="   Email">
+            <label for="mobile" class="add_new_staff_member_label">Mobile no</label>
+            <input type="text" id="mobile" name="mobile" class="add_new_staff_member_input">
 
-            <!-- <label for="mobile" class="add_new_staff_member_label">Mobile no</label> -->
-            <input type="text" id="mobile" name="mobile" class="add_new_staff_member_input" placeholder="   Mobile Number">
-
-            <!-- <label for="tele" class="add_new_staff_member_label">Telephone no</label> -->
-            <input type="text" id="tele" name="tele" class="add_new_staff_member_input" placeholder="   Telephone Number">
+            <label for="tele" class="add_new_staff_member_label">Telephone no</label>
+            <input type="text" id="tele" name="tele" class="add_new_staff_member_input">
 
             <div class="submit-reset-buttons">
                 <div>
