b498ac09cd7ed5d2f380ea27e323dcec2403e425
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-04-26 13:29:51 +0530

    create buses search function model in M_Admin_bus_details

--- app/models/M_Admin_bus_details.php
+++ app/models/M_Admin_bus_details.php
@@ -28,5 +28,15 @@ class M_Admin_bus_details {
 
     }
 
+
+    //for search
+
+    public function getSearchBuses($search)
+    {
+      $this->db->query("SELECT * FROM bus WHERE bus_no LIKE '%$search%' OR root_no LIKE '%$search%' OR capacity LIKE '%$search%' OR con_ntc LIKE '%$search' OR dri_ntc LIKE '%$search' OR owner_nic LIKE '%$search%' OR ratings LIKE '%$search%' OR total_ratings LIKE '%$search%' OR responses LIKE '%$search%'");
+      $result=$this->db->resultSet();
+      return $result;
+    }
+
 }
 ?>
