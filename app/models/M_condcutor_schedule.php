b31a0fa62acdfd7ff38f559b1e6e3096d4505d04
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-04-27 10:40:39 +0530

    commits

--- app/models/M_condcutor_schedule.php
+++ app/models/M_condcutor_schedule.php
@@ -10,7 +10,19 @@ class M_conductor_schedule
         $this->db = new Database;
     }
 
-   public function view_schedule($input){
+
+    public function find_bus_no(){
+        // prepare query
+        
+        $this->db->query('SELECT * from conductor WHERE  ntcNo = :ntcNo');
+        $id = $_SESSION['user_id'];
+        $this->db->bind(':ntcNo',$id);
+        $results = $this->db->single();
+        return $results;
+
+    } 
+
+   public function view_schedule($bus_no){
 
     $this->db->query('SELECT * from bookings WHERE booking_code = :input ');
 
