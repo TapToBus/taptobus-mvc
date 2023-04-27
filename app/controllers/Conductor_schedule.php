407e91e94e189424f70552b0e72b1d673281e077
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-04-27 10:11:29 +0530

    create con schedule controller

---
+++ app/controllers/Conductor_schedule.php
@@ -0,0 +1,23 @@
+<?php
+
+class Conductor_schedule extends Controller{
+
+    private $scheduleModel;
+
+    public function __construct()
+    {
+      if(! isLoggedIn()){
+        direct('users/login');
+    }
+
+      $this->scheduleModel = $this->model('m_conductor_schedule');
+    }
+
+    public function view_schedule(){
+      
+
+      $this->view('conductor/view_schedule');
+    }
+}
+
+?>
