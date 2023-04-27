fb11ae5d8e41224862f40ba19c7ff5f42dd9a307
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-04-27 15:25:01 +0530

    commits

--- app/views/conductor/view_schedule.php
+++ app/views/conductor/view_schedule.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" /> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/conductor-style/view_schedule-style.css">
     <title><?php echo SITENAME; ?></title>
 
@@ -18,12 +18,37 @@
 
     <div class="container">
 
-    
-        
+        <div class="table">
 
+            <div class="row header">
+                <div>From</div>
+                <div>To</div>
+                <div>Time</div>
+                <div>Bus No</div>
+                <div>Action</div>
+            </div>
+
+            <?php
+                foreach($data as $row):
+            ?>
+
+            <div class="row">
+                <div><?php echo $row->date; ?></div>
+                <div>Boston</div>
+                <div>8:00 AM</div>
+                <div>123</div>
+                <div><button class="btn">Book Now</button></div>
+            </div>
+
+            <?php
+               endforeach;
+            ?>
+
+
+        </div>
     </div>
 
-    
+
 </body>
 
 </html>
