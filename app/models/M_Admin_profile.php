e09bd4f59db6bb09bb9ff032937d0094522ca310
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-04-30 18:47:06 +0530

    create admin model for admin profile

--- app/models/M_Admin_profile.php
+++ app/models/M_Admin_profile.php
@@ -7,8 +7,14 @@ class M_admin_profile{
         $this->db = new Database;
     }
 
-    public function getAdminProfileDetails($admin_nic){
-        $this->db->query('SELECT admin_id, nic, fname, lname, email, mobileNo, TelNo');
+    public function getAdminProfileDetails($admin_id){
+        $this->db->query('SELECT * FROM admin WHERE admin_id = :admin_id;');
+
+        $this->db->bind(':admin_id',$admin_id);
+
+        $result= $this->db->single();
+
+        return $result;
     }
 }
 ?>
