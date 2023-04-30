de8c79334a4c61adfd9b31af158f56850f1acfab
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-04-30 09:25:50 +0530

    create admin report Date_From_Date_To controller function

--- app/controllers/Admin_reports.php
+++ app/controllers/Admin_reports.php
@@ -2,11 +2,16 @@
 
 class Admin_reports extends Controller{
 
+    private $dateFromDateToModel;
+
     public function __construct()
     {
+        //check if admin is logined to the system
         if(!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'admin'){
             direct('user/logout');
         }
+
+        $this->dateFromDateToModel = $this->model('M_Admin_reports');
         
     }
 
@@ -21,7 +26,20 @@ class Admin_reports extends Controller{
     }
 
     public function Date_From_Date_To(){
-        
+
+        //check if form is submitted
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+            //get data form data array
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+
+            $data=[
+                'date_from' => trim($_POST['date_from']),
+                'date_to' => trim($_POST['date_to']),
+            ];
+
+        }
+
     }
 }
 
