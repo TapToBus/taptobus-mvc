1f2340b34d855ab0a19715cf4ccd72a787b34d71
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-05-01 16:09:24 +0530

    Update select_seats

--- app/views/passenger/select_seats.php
+++ app/views/passenger/select_seats.php
@@ -16,7 +16,103 @@
         <div class="content">
             <h1 class="heading">Seat Arrangement</h1>
 
-            
+            <div class="details">
+                <div class="left">
+                    <div class="front">Front</div>
+
+                    <div class="seats">
+                        <!-- <?php $line = 1;
+                                $j = 0; ?>
+                        <?php while ($line <= (33 - 5) / 4) : ?>
+                            <div class="row">
+                                <span class="seat col1"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                                <span class="seat col2"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                                <span class="seat col3"></span>
+                                <span class="seat col4"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                                <span class="seat col5"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                            </div>
+                            <?php $line++; ?>
+                        <?php endwhile; ?>
+                        <div class="row">
+                            <span class="seat col1"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                            <?php $j++; ?>
+                            <span class="seat col2"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                            <?php $j++; ?>
+                            <span class="seat col3"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                            <?php $j++; ?>
+                            <span class="seat col4"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                            <?php $j++; ?>
+                            <span class="seat col5"><?php echo $line + $j; ?><i class="fas fa-square"></i></span>
+                        </div> -->
+
+                        <?php $line = 1;
+                        $j = 0; ?>
+                        <?php while ($line <= (33 - 5) / 4) : ?>
+
+                            <div class="row">
+                                <span class="col">
+                                    <span class="snum"><?php echo $line + $j; ?></span>
+                                    <span class="seat"><i class="fas fa-square"></i></span>
+                                    <?php $j++; ?>
+                                </span>
+                                <span class="col">
+                                    <span class="snum"><?php echo $line + $j; ?></span>
+                                    <span class="seat"><i class="fas fa-square"></i></span>
+                                    <?php $j++; ?>
+                                </span>
+                                <span class="col">
+                                    <!--  -->
+                                </span>
+                                <span class="col">
+                                    <span class="snum"><?php echo $line + $j; ?></span>
+                                    <span class="seat"><i class="fas fa-square"></i></span>
+                                    <?php $j++; ?>
+                                </span>
+                                <span class="col">
+                                    <span class="snum"><?php echo $line + $j; ?></span>
+                                    <span class="seat"><i class="fas fa-square"></i></span>
+                                </span>
+                            </div>
+
+                            <?php $line++; ?>
+                        <?php endwhile; ?>
+
+                        <div class="row">
+                            <span class="col">
+                                <span class="snum"><?php echo $line + $j; ?></span>
+                                <span class="seat"><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                            </span>
+                            <span class="col">
+                                <span class="snum"><?php echo $line + $j; ?></span>
+                                <span class="seat"><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                            </span>
+                            <span class="col">
+                                <span class="snum"><?php echo $line + $j; ?></span>
+                                <span class="seat"><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                            </span>
+                            <span class="col">
+                                <span class="snum"><?php echo $line + $j; ?></span>
+                                <span class="seat"><i class="fas fa-square"></i></span>
+                                <?php $j++; ?>
+                            </span>
+                            <span class="col">
+                                <span class="snum"><?php echo $line + $j; ?></span>
+                                <span class="seat"><i class="fas fa-square"></i></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right">
+                    <h1>Hello</h1>
+                </div>
+            </div>
         </div>
     </div>
 
