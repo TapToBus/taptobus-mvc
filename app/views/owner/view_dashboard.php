33be050b48eac142853f15bf81f885f332f04025
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-05-01 09:00:04 +0530

    commits

--- app/views/owner/view_dashboard.php
+++ app/views/owner/view_dashboard.php
@@ -33,41 +33,40 @@
             <div>
                 <h2>Weekly Income</h2>
             </div>
+            <div id="my-data" data="{{ json_encode($data) }}"></div>
             <canvas id="myChart"></canvas>
 
             <script>
                 var ctx = document.getElementById('myChart').getContext('2d');
-                var xValues = ['2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23'];
+                // var xValues = ['2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23', '2023-02-23'];
+
+                var data = JSON.parse(document.getElementById('my-data').getAttribute('data'));
+                var datasets = [];
+               
+
+                Object.entries(data).forEach(([bus_no, bus_data]) => {
+                    datasets.push({
+                        label: 'Bus ' + bus_no,
+                        data: Object.values(bus_data),
+                        borderColor: '#22a7f0',
+                        fill: false
+                    });
+                });
 
                 new Chart("myChart", {
-                  type: "line",
-                  data: {
-                    responsive: true,
-                    labels: xValues,
-                    datasets: [{
-                      label: 'NB-1345',
-                      data: [100000, 90000, 80000, 60000, 60000, 50000, 40000],
-                      borderColor: "#22a7f0",
-                      fill: false
-                    }, {
-                      label: 'ND-1245',
-                      data: [40000, 60000, 70000, 80000, 70000, 60000, 50000],
-                      borderColor: "#48b5c4",
-                      fill: false
-                    }, {
-                      label: 'NC-5345',
-                      data: [50000, 50000, 50000, 50000, 55000, 90000, 65000],
-                      borderColor: "#a6d75b",
-                      fill: false
-                    }]
-                  },
-                  options: {
-                    legend: {
-                      display: true,
-                      position: 'bottom'
+                    type: "line",
+                    data: {
+                        responsive: true,
+                        labels:  Object.keys(data[1]),
+                        datasets: datasets
                     },
-                    aspectRatio: 1.7
-                  }
+                    options: {
+                        legend: {
+                            display: true,
+                            position: 'bottom'
+                        },
+                        aspectRatio: 1.7
+                    }
                 });
             </script>
 
@@ -124,7 +123,7 @@
 
     </div>
 
-    <script type="text/javascript " src="<?php echo URLROOT; ?>/js/owner-js/dashboard.js"> </script>
+    <!-- <script type="text/javascript " src="<?php echo URLROOT; ?>/js/owner-js/dashboard.js"> </script> -->
 </body>
 
 </html>
