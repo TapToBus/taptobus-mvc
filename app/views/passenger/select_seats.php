426df70ff2a97b22901da123238a4b70def43c91
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-05-01 22:59:37 +0530

    Add js part to select_seats part

--- app/views/passenger/select_seats.php
+++ app/views/passenger/select_seats.php
@@ -158,6 +158,26 @@
         }
 
         
+        document.addEventListener('DOMContentLoaded', function() {
+            var selectElements = document.querySelectorAll('select');
+            for (var i = 0; i < selectElements.length; i++) {
+                selectElements[i].addEventListener('change', function(event) {
+                    var selectedOption = event.target.value;
+                    var selectElements = document.querySelectorAll('select');
+                    for (var j = 0; j < selectElements.length; j++) {
+                        if (selectElements[j] !== event.target) {
+                            var optionElements = selectElements[j].querySelectorAll('option');
+                            for (var k = 1; k < optionElements.length; k++) {
+                                var optionValue = optionElements[k].value;
+                                if (optionValue === selectedOption) {
+                                    optionElements[k].remove();
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        });
     </script>
 </body>
 
