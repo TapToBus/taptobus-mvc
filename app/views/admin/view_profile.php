42afee788cf4f35a429568cf6549cebb7d756981
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-05-01 13:44:37 +0530

    create profile and profile styles

--- app/views/admin/view_profile.php
+++ app/views/admin/view_profile.php
@@ -15,68 +15,66 @@
     <?php require APPROOT . '/views/inc/admin_navbar.php' ?>
 
     <div class="main">
-        
-            <h1 class="heading">PROFILE</h1>
 
-            <div class="pro-details">
-                <div class="left">
-                    <img src="<?php echo URLROOT; ?>/img/profile-pic/AdminProfilePicture.png">
-                </div>
+        <h1 class="heading">PROFILE</h1>
 
-                <div class="right">
+        <div class="pro-details">
+            <div class="left">
+                <img src="<?php echo URLROOT; ?>/img/profile-pic/AdminProfilePicture.png">
+            </div>
 
-                    <div class="details">
+            <div class="right">
 
-                        <div class="row">
-                            <span class="col1">Admin ID: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->admin_id; ?></span>
-                        </div>
+                <div class="details">
 
-                        <div class="row">
-                            <span class="col1">NIC: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->nic; ?></span>
-                        </div>
-
-                        <div class="row">
-                            <span class="col1">First Name: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->fname; ?></span>
-                        </div>
-
-                        <div class="row">
-                            <span class="col1">Last Name: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->lname; ?></span>
-                        </div>
-
-                        <div class="row">
-                            <span class="col1">Email: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->email; ?></span>
-                        </div>
-
-                        <div class="row">
-                            <span class="col1">Mobile Number: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->mobileNo; ?></span>
-                        </div>
-
-                        <div class="row">
-                            <span class="col1">Telephone Number: </span>
-                            <span class="col2"><?php echo $data['ProfileDetails']->telNo; ?></span>
-                        </div>
-
+                    <div class="row">
+                        <span class="col1">Admin ID: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->admin_id; ?></span>
                     </div>
 
+                    <div class="row">
+                        <span class="col1">NIC: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->nic; ?></span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col1">First Name: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->fname; ?></span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col1">Last Name: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->lname; ?></span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col1">Email: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->email; ?></span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col1">Mobile Number: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->mobileNo; ?></span>
+                    </div>
+
+                    <div class="row">
+                        <span class="col1">Telephone Number: </span>
+                        <span class="col2"><?php echo $data['ProfileDetails']->telNo; ?></span>
+                    </div>
+
+                </div>
+
+                <div class="edit-profile-delete-profile-buttons">
                     <div class="btn">
                         <button class="delete">Delete Profile</button>
-                        <a href="<?= URLROOT; ?>/admin_profile/edit_profile"><button class="edit">Edit profile</button></a>
-
+                        <button class="edit">Edit profile</button></a>
                     </div>
+
                 </div>
+
+
             </div>
-        
-
-    </div>
-
-
-
+        </div>
     </div>
 </body>
 
