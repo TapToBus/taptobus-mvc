3bbe2a29fb41f5ab4d495664cb7ed68f4dc79d3a
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-05-02 18:15:13 +0530

    Add js to journey_details

--- app/views/passenger/journey_details.php
+++ app/views/passenger/journey_details.php
@@ -63,55 +63,6 @@
         </form>
     </div>
 
-    <!-- <script src="<?php echo URLROOT; ?>/js/passenger-js/journey-details-js.js"></script> -->
-    
-    <script>
-        const fromDropdown = document.getElementById("from");
-        const toDropdown = document.getElementById("to");
-
-
-        // handle changes to "From" dropdown
-        fromDropdown.addEventListener("change", function() {
-            const selectedValue = fromDropdown.value;
-
-            for (let i = 0; i < toDropdown.options.length; i++) {
-                if (toDropdown.options[i].value === selectedValue) {
-                    toDropdown.remove(i);
-                    break;
-                }
-            }
-        });
-
-
-        // handle changes to "To" dropdown
-        toDropdown.addEventListener("change", function() {
-            const selectedValue = toDropdown.value;
-
-            for (let i = 0; i < fromDropdown.options.length; i++) {
-                if (fromDropdown.options[i].value === selectedValue) {
-                    fromDropdown.remove(i);
-                    break;
-                }
-            }
-        });
-
-
-        // handle date
-
-        // get the current date
-        const today = new Date().toISOString().split('T')[0];
-
-        // set the minimum date for the input field
-        document.getElementById('date').setAttribute('min', today);
-
-        // calculate the maximum date (2 weeks from now)
-        var maxDate = new Date();
-        maxDate.setDate(maxDate.getDate() + 13);
-        var maxDateStr = maxDate.toISOString().split('T')[0];
-
-        // Set the maximum date for the input field
-        document.getElementById('date').setAttribute('max', maxDateStr);
-    </script>
-
+    <script src="<?php echo URLROOT; ?>/js/passenger-js/journey-details-js.js"></script>
 </body>
 </html>
--- public/js/passenger-js/journey-details-js.js
+++ public/js/passenger-js/journey-details-js.js
@@ -1,45 +1,43 @@
-/*const fromDropdown = document.getElementById("from");
+const fromDropdown = document.getElementById("from");
 const toDropdown = document.getElementById("to");
-
 
 // handle changes to "From" dropdown
 fromDropdown.addEventListener("change", function () {
-    const selectedValue = fromDropdown.value;
+  const selectedValue = fromDropdown.value;
 
-    for (let i = 0; i < toDropdown.options.length; i++) {
-        if (toDropdown.options[i].value === selectedValue) {
-            toDropdown.remove(i);
-            break;
-        }
+  for (let i = 0; i < toDropdown.options.length; i++) {
+    if (toDropdown.options[i].value === selectedValue) {
+      toDropdown.remove(i);
+      break;
     }
+  }
 });
-
 
 // handle changes to "To" dropdown
 toDropdown.addEventListener("change", function () {
-    const selectedValue = toDropdown.value;
-    
-    for (let i = 0; i < fromDropdown.options.length; i++) {
-        if (fromDropdown.options[i].value === selectedValue) {
-            fromDropdown.remove(i);
-            break;
-        }
+  const selectedValue = toDropdown.value;
+
+  for (let i = 0; i < fromDropdown.options.length; i++) {
+    if (fromDropdown.options[i].value === selectedValue) {
+      fromDropdown.remove(i);
+      break;
     }
+  }
 });
 
 
 // handle date
 
 // get the current date
-const today = new Date().toISOString().split('T')[0];
+const today = new Date().toISOString().split("T")[0];
 
 // set the minimum date for the input field
-document.getElementById('date').setAttribute('min', today);
+document.getElementById("date").setAttribute("min", today);
 
 // calculate the maximum date (2 weeks from now)
 var maxDate = new Date();
 maxDate.setDate(maxDate.getDate() + 13);
-var maxDateStr = maxDate.toISOString().split('T')[0];
+var maxDateStr = maxDate.toISOString().split("T")[0];
 
 // Set the maximum date for the input field
-document.getElementById('date').setAttribute('max', maxDateStr);*/
+document.getElementById("date").setAttribute("max", maxDateStr);
