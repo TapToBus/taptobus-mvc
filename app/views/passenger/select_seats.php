46819d219471eae0a5f12dca8a519b98325ab7c2
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-05-02 18:25:54 +0530

    Add js to the select_seats

--- app/views/passenger/select_seats.php
+++ app/views/passenger/select_seats.php
@@ -160,34 +160,6 @@
         </div>
     </div>
 
-    <!-- <script src="<?php echo URLROOT; ?>/js/passenger-js/select-seats-js.js"></script> -->
-    <script>
-        function goBack() {
-            window.history.back();
-        }
-
-
-        document.addEventListener('DOMContentLoaded', function() {
-            var selectElements = document.querySelectorAll('select');
-            for (var i = 0; i < selectElements.length; i++) {
-                selectElements[i].addEventListener('change', function(event) {
-                    var selectedOption = event.target.value;
-                    var selectElements = document.querySelectorAll('select');
-                    for (var j = 0; j < selectElements.length; j++) {
-                        if (selectElements[j] !== event.target) {
-                            var optionElements = selectElements[j].querySelectorAll('option');
-                            for (var k = 1; k < optionElements.length; k++) {
-                                var optionValue = optionElements[k].value;
-                                if (optionValue === selectedOption) {
-                                    optionElements[k].remove();
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-        });
-    </script>
+    <script src="<?php echo URLROOT; ?>/js/passenger-js/select-seats-js.js"></script>
 </body>
-
 </html>
--- public/js/passenger-js/select-seats-js.js
+++ public/js/passenger-js/select-seats-js.js
@@ -1,3 +1,25 @@
 function goBack(){
     window.history.back();
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var selectElements = document.querySelectorAll('select');
+    for (var i = 0; i < selectElements.length; i++) {
+        selectElements[i].addEventListener('change', function(event) {
+            var selectedOption = event.target.value;
+            var selectElements = document.querySelectorAll('select');
+            for (var j = 0; j < selectElements.length; j++) {
+                if (selectElements[j] !== event.target) {
+                    var optionElements = selectElements[j].querySelectorAll('option');
+                    for (var k = 1; k < optionElements.length; k++) {
+                        var optionValue = optionElements[k].value;
+                        if (optionValue === selectedOption) {
+                            optionElements[k].remove();
+                        }
+                    }
+                }
+            }
+        });
+    }
+});
