1ac2bab6b56159587952bedd2f8008fccf60e79a
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-05-02 23:39:11 +0530

    Add payment part

--- app/controllers/Passenger_book_seats.php
+++ app/controllers/Passenger_book_seats.php
@@ -220,6 +220,49 @@ class Passenger_book_seats extends Controller{
 
 
     public function payment(){
-        $this->view('passenger/payment');
+        require_once APPROOT . '/stripe/vendor/autoload.php';
+
+        \Stripe\Stripe::setApiKey(STRIPE_SK);
+
+        /*function calculateOrderAmount(array $items): int{
+            // Replace this constant with a calculation of the order's amount
+            // Calculate the order total on the server to prevent
+            // people from directly manipulating the amount on the client
+            return 1400;
+        }*/
+
+        header('Content-Type: application/json');
+
+        try {
+            // retrieve JSON from POST body
+            $jsonStr = file_get_contents('php://input');
+            //$jsonObj = json_decode($jsonStr);
+
+            // Create a PaymentIntent with amount and currency
+            $paymentIntent = \Stripe\PaymentIntent::create([
+                'amount' => 300*100,
+                'currency' => 'lkr',
+                'automatic_payment_methods' => [
+                    'enabled' => true,
+                ],
+            ]);
+
+            $output = [
+                'clientSecret' => $paymentIntent->client_secret,
+            ];
+
+            echo json_encode($output);
+        } catch (Error $e) {
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+
+        //$this->view('passenger/payment');
+    }
+
+
+
+    public function paid(){
+        echo 'Paid success';
     }
 }
