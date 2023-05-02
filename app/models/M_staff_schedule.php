a9206fa1f23fce55b1d6293c95315f23cc108f96
Dasuni Dewani <dasunidewani1998@gmail.com>
2023-05-02 16:44:40 +0530

    completed schedule

--- app/models/M_staff_schedule.php
+++ app/models/M_staff_schedule.php
@@ -21,9 +21,39 @@
         }
 
         public function viewSchedules(){
-            $this->db->query("SELECT * FROM schedule order by day ");
+            $this->db->query("SELECT s.schedule_id, s.bus_no, s.Location_from, s.Location_to, s.day, s.arrival_time, s.departure_time, s.ticket_price, b.id as bid FROM schedule s LEFT JOIN bookings b ON s.bus_no = b.bus_no order by day ");
             return $this->db->resultSet();
 
         }
+
+        public function updateSchedule($data){
+            $this->db->query("UPDATE schedule SET bus_no = :bus_no, Location_from = :location_from, Location_to = :location_to, day = :day, arrival_time = :arrival_time, departure_time = :departure_time, ticket_price = :ticket_price WHERE schedule_id = :schedule_id");
+            $this->db->bind(":bus_no",$data['bus_no']);
+            $this->db->bind(":location_from",$data['Location_from']);
+            $this->db->bind(":location_to",$data['Location_to']);
+            $this->db->bind(":day",$data['day']);
+            $this->db->bind(":arrival_time",$data['arrival_time']);
+            $this->db->bind(":departure_time",$data['depature_time']);
+            $this->db->bind(":ticket_price",$data['ticket_price']);
+            $this->db->bind(":schedule_id",$data['schedule_id']);
+            return $this->db->execute();
+        }
+
+        public function deleteSchedule($schedule_id){
+            $this->db->query("DeLETE FROM schedule WHERE schedule_id = :schedule_id");
+            $this->db->bind(":schedule_id",$schedule_id);
+            return $this->db->execute();
+        }
+
+        // public function checkBusInBooking($bus_no){
+        //     $this->db->query("SELECT * FROM bookings WHERE bus_no = :bus_no");
+        //     $this->db->bind(":bus_no",$bus_no);
+        //     $res = $this->db->resultSet();
+        //     if($res){
+        //         return false;
+        //     }else{
+        //         return true;
+        //     }
+        // }
     }
 ?>
--- app/views/staff/staff_schedule.php
+++ app/views/staff/staff_schedule.php
@@ -8,8 +8,146 @@
     <link rel="stylesheet" href="<?php echo URLROOT;?>/css/main.css"/>
     <link rel="stylesheet" href="<?php echo URLROOT;?>/css/staff-style/staffnavbar-style.css" />
     <link rel="stylesheet" href="<?php echo URLROOT;?>/css/staff-style/staffschedule-style.css" />
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/staff-style/staffcreateschedulemodal-style.css" />
 </head>
 <body>
+
+
+<dialog id="addNewScheduleModal" class="modal">
+        <div class="modal__container">
+            <div class="caption">
+                <h1>Add New Schedule</h1>
+            </div>
+            <form action="<?php echo URLROOT?>/Staff_schedule/create_schedule" method="POST">
+                <div class="form_control">
+                    <label for="bus_no">Bus No</label>
+                    <input type="text" name="bus_no" id="bus_no"/>
+                </div>
+                <div class="row">
+                    <div class="form_control">
+                        <label for="bus_no">Location From</label>
+                        <select name="location_from" id="location_from">
+                            <option value="Galle">Galle</option>
+                            <option value="Makubura">Makubura</option>
+                        </select>
+                    </div>
+                    <div class="form_control">
+                        <label for="bus_no">Location To</label>
+                        <select name="location_to" id="location_to">
+                            <option value="Galle">Galle</option>
+                            <option value="Makubura">Makubura</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form_control">
+                    <label for="bus_no">Day</label>
+                    <select name="day" id="day">
+                        <option value="Monday">Monday</option>
+                        <option value="Tuesday">Tuesday</option>
+                        <option value="Wednessday">Wednessday</option>
+                        <option value="Thursday">Thursday</option>
+                        <option value="Friday">Friday</option>
+                        <option value="Satureday">Satureday</option>
+                        <option value="Sunday">Sunday</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <div class="form_control">
+                        <label for="arrival_time">Arrival Time</label>
+                        <input type="time" name="arrival_time" id="arrival_time"/>
+                    </div>
+                    <div class="form_control">
+                        <label for="departrue_time">Departure Time</label>
+                        <input type="time" name="departrue_time" id="departrue_time"/>
+                    </div>
+                </div>
+                <div class="form_control">
+                    <label for="ticket_price">Ticket Price</label>
+                    <input type="text" name="ticket_price" id="ticket_price"/>
+                </div>
+                <div class="button_control">
+                    <button type="button" onclick="closeAddNewScheduleModal()" >Cancel</button>
+                    <button type="submit" name="createScheduleBtn" >Create</button>
+                </div>
+            </form>
+        </div>
+    </dialog>
+
+    <dialog id="deleteConfirmModal" class="alertModal">
+        <div class="alert__container">
+                <i class="fa-solid fa-circle-xmark"></i>
+                <h1>Are you sure?</h1>
+                <p>Do you realy want to delete this schedule? this process can't be undone.</p>
+            <form class="button_control" action="<?php echo URLROOT?>/Staff_schedule/delete_schedule" method="POST">
+                <button type="button" onclick="closeDeleteConfirmationModal()" >No, Cancel</button>
+                <input type="hidden" name="bus_no" id="d-bus_no" />
+                <button type="submit" name="deleteBtn" id="deleteBtn" >Yes, Delete</button>
+            </form>
+        </div>
+    </dialog>
+
+
+    <dialog id="editScheduleModal" class="modal">
+        <div class="modal__container">
+            <div class="caption">
+                <h1>Edit Schedule</h1>
+            </div>
+            <form action="<?php echo URLROOT?>/Staff_schedule/edit_schedule" method="POST">
+                <div class="form_control">
+                    <label for="bus_no">Bus No</label>
+                    <input type="text" name="bus_no" id="u-bus_no"/>
+                </div>
+                <div class="row">
+                    <div class="form_control">
+                        <label for="bus_no">Location From</label>
+                        <select name="location_from" id="u-location_from">
+                            <option value="Galle">Galle</option>
+                            <option value="Makubura">Makubura</option>
+                        </select>
+                    </div>
+                    <div class="form_control">
+                        <label for="bus_no">Location To</label>
+                        <select name="location_to" id="u-location_to">
+                            <option value="Galle">Galle</option>
+                            <option value="Makubura">Makubura</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form_control">
+                    <label for="u-day">Day</label>
+                    <select name="day" id="u-day">
+                        <option value="Monday">Monday</option>
+                        <option value="Tuesday">Tuesday</option>
+                        <option value="Wednessday">Wednessday</option>
+                        <option value="Thursday">Thursday</option>
+                        <option value="Friday">Friday</option>
+                        <option value="Satureday">Satureday</option>
+                        <option value="Sunday">Sunday</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <div class="form_control">
+                        <label for="arrival_time">Arrival Time</label>
+                        <input type="time" name="arrival_time" id="u-arrival_time"/>
+                    </div>
+                    <div class="form_control">
+                        <label for="departrue_time">Departure Time</label>
+                        <input type="time" name="departrue_time" id="u-departrue_time"/>
+                    </div>
+                </div>
+                <div class="form_control">
+                    <label for="ticket_price">Ticket Price</label>
+                    <input type="text" name="ticket_price" id="u-ticket_price"/>
+                </div>
+                <div class="button_control">
+                    <button type="button" onclick="closeEditScheduleModal()" >Cancel</button>
+                    <button type="submit" name="editScheduleBtn" id="editBtn" >Save</button>
+                </div>
+            </form>
+        </div>
+    </dialog>
+
+
     <?php require APPROOT . '/views/inc/staff_navbar.php' ?>    
 
     <div class="container"> 
@@ -17,18 +155,10 @@
             <div class="top">
                 <h2>Bus Schedule</h2>
                 <div class="add-edit-btns">
-                    <a href="<?php echo URLROOT?>/Staff_schedule/view_create_form">
-                        <button class="button1">
-                            <span class="button__text">Add New</span>
-                            <span class="button__icon"><i class="fa-solid fa-circle-plus"></i></span>
-                        </button>                    
-                    </a> 
-                    <a href="#">
-                        <button class="button2">
-                            <span class="button__text">Edit</span>
-                            <span class="button__icon"><i class="fa-solid fa-pen"></i></span>
-                        </button>                    
-                    </a> 
+                    <button class="addNew__button" onclick="openAddNewScheduleModal()">
+                        <i class="fa-solid fa-circle-plus"></i>
+                        <p>Add New</p>
+                    </button>
                 </div> 
             </div>             
             <table class="content-tabel">
@@ -42,6 +172,7 @@
                         <th>Arrival time</th>
                         <th>Departure time</th>
                         <th>Ticket Price (LKR)</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -59,6 +190,10 @@
                         <td><?php echo $schedule->arrival_time?></td>
                         <td><?php echo $schedule->departure_time?></td>
                         <td><?php echo $schedule->ticket_price.'.00'?></td>
+                        <td>
+                            <button class="table__button" <?php if(isset($schedule->bid)) echo "disabled"?> onclick='openEditScheduleModal(<?php echo json_encode($schedule)?>)'><i class="fa fa-edit"></i></button>
+                            <button class="table__button" <?php if(isset($schedule->bid)) echo "disabled"?> onclick= 'openDeleteConfirmationModal("<?php echo $schedule->schedule_id ?>", "<?php echo $schedule->bus_no ?>")'><i class="fa fa-trash"></i></button>
+                        </td>
                     </tr>
                     <?php
                 }
@@ -67,6 +202,49 @@
             </table>
         </div>
     </div>
-        
+        <script>
+
+            function openAddNewScheduleModal(){
+                const addNewScheduleModal = document.getElementById('addNewScheduleModal')
+                addNewScheduleModal.showModal()
+            }
+
+            function closeAddNewScheduleModal(){
+                const addNewScheduleModal = document.getElementById('addNewScheduleModal')
+                addNewScheduleModal.close()
+            }
+
+            function openEditScheduleModal(data){
+                console.log(data);
+                const editScheduleModal = document.getElementById('editScheduleModal')
+                document.getElementById('u-bus_no').value = data.bus_no
+                document.getElementById('u-location_from').value = data.Location_from
+                document.getElementById('u-location_to').value = data.Location_to
+                document.getElementById('u-day').value = data.day
+                document.getElementById('u-arrival_time').value = data.arrival_time
+                document.getElementById('u-departrue_time').value = data.departure_time
+                document.getElementById('u-ticket_price').value = data.ticket_price
+                document.getElementById('editBtn').value = data.schedule_id
+                editScheduleModal.showModal()
+            }
+
+            function closeEditScheduleModal(){
+                const editScheduleModal = document.getElementById('editScheduleModal')
+                editScheduleModal.close()
+            }
+
+            function openDeleteConfirmationModal(id, bus_no) {
+                const deleteConfirmModal = document.getElementById('deleteConfirmModal')
+                document.getElementById('d-bus_no').value = bus_no
+                document.getElementById('deleteBtn').value = id
+                deleteConfirmModal.showModal()
+            }
+
+            function closeDeleteConfirmationModal(){
+                const deleteConfirmModal = document.getElementById('deleteConfirmModal')
+                deleteConfirmModal.close()
+            }
+
+        </script>
 </body>
 </html>
