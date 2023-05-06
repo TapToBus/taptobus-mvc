ed53969290392d4bf8fae86160135d38542dd835
Kithsandu Rathnayake <kithsandurathnayake@gmail.com>
2023-05-06 23:11:07 +0530

    owner table addition column bus count driver count  conductor count view model

--- app/models/M_Admin_bus_owner_details.php
+++ app/models/M_Admin_bus_owner_details.php
@@ -7,7 +7,9 @@
     }
 
     public function getowners(){
-        $this->db->query('SELECT * FROM owner WHERE status = "active"');
+        // $this->db->query('SELECT * FROM owner WHERE status = "active"');
+
+        $this->db->query('SELECT o.nic, o.fname, o.lname, o.email, o.mobileNo, COUNT(DISTINCT b.bus_no) AS bus_count, COUNT(DISTINCT c.ntcNo) AS conductor_count, COUNT(DISTINCT d.ntcNo) AS driver_count FROM owner o LEFT JOIN bus b ON o.nic = b.owner_nic LEFT JOIN conductor c ON o.nic = c.owner_nic LEFT JOIN driver d ON o.nic = d.owner_nic WHERE o.status = "active" GROUP BY o.nic ;');
 
         return $this->db->resultSet();
     }
@@ -23,7 +25,9 @@
 
     public function getSearchBusOwners($search)
       {
-        $this->db->query("SELECT * FROM owner WHERE CONCAT(fname,lname) LIKE '%$search%' OR nic LIKE '%$search%' OR mobileNo LIKE '%$search' OR email LIKE '%$search'");
+        // $this->db->query("SELECT * FROM owner WHERE CONCAT(fname,lname) LIKE '%$search%' OR nic LIKE '%$search%' OR mobileNo LIKE '%$search' OR email LIKE '%$search'");
+       
+        $this->db->query("SELECT o.nic, o.fname, o.lname, o.email, o.mobileNo, COUNT(DISTINCT b.bus_no) AS bus_count, COUNT(DISTINCT c.ntcNo) AS conductor_count, COUNT(DISTINCT d.ntcNo) AS driver_count FROM owner o LEFT JOIN bus b ON o.nic = b.owner_nic LEFT JOIN conductor c ON o.nic = c.owner_nic LEFT JOIN driver d ON o.nic = d.owner_nic WHERE CONCAT(o.fname, o.lname) LIKE '%$search%' OR o.nic LIKE '%$search%' OR o.email LIKE '%$search%' OR o.mobileNo LIKE '%$search%'");
         $result=$this->db->resultSet();
         return $result;
       }
