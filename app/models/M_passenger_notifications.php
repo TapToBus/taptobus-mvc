e4d641120f6d90233427c2cb59169380f8a87dcd
Adheesha <adheeshachamoddesilva@gmail.com>
2023-05-06 23:54:30 +0530

    Update M_passenger_notifications.php

--- app/models/M_passenger_notifications.php
+++ app/models/M_passenger_notifications.php
@@ -9,7 +9,7 @@ class M_passenger_notifications{
 
 
     public function getNotifications() {
-        $this->db->query('SELECT s.notice_id, s.title, s.description, DATE(time_stamp) as date, TIME(time_stamp) as time
+        $this->db->query('SELECT s.notice_id, s.title, s.description, DATE(date_from) as date, TIME(time_stamp) as time
                 FROM special_notices s
                 INNER JOIN notice_available_users nau
                 ON s.notice_id = nau.notice_id 
