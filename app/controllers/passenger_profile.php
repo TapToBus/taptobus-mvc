b795e3e9ac0c5c5f73a5f343e9444fac8aa004ce
Adheesha <adheeshachamoddesilva@gmail.com>
2023-05-08 02:26:33 +0530

    Update edit profile

--- app/controllers/passenger_profile.php
+++ app/controllers/passenger_profile.php
@@ -25,68 +25,11 @@ class Passenger_profile extends Controller{
     }
 
 
-    public function edit_profile(){
+    /*public function edit_profile(){
         if($_SERVER['REQUEST_METHOD'] == 'POST'){ 
-            // $data = [
-            //     'pic' => '',
-            //     'fname' => '',
-            //     'lname' => '',
-            //     'curr_pwd' => '',
-            //     'new_pwd' => '',
-            //     'confirm_pwd' => '',
-            //     'fname_err' => '',
-            //     'lname_err' => '',
-            //     'curr_pwd_err' => '',
-            //     'new_pwd_err' => '',
-            //     'confirm_pwd_err' => ''
-            // ];
-
-
-            // // set profile pic
-
-            // if(! empty($_POST['pic'])){
-            //     $data['pic'] = $_POST['pic'];
-            // }else{
-            //     $passenger = $this->profileModel->getPassengerDetails($_SESSION['user_id']);
-            //     $data['pic'] = $passenger->pic;
-            // }
-
-
-            // // set fname / validate fname
-
-            // $data['fname'] = $_POST['fname'];
-
-            // if(! preg_match("/^[a-zA-Z]+$/", $data['fname']) || strlen($data['fname']) < 3){
-            //     $data['fname_err'] = "A valid first name is required";
-            // }
-
-
-            // // set lname / validate lname
-
-            // $data['lname'] = $_POST['lname'];
-
-            // if(! preg_match("/^[a-zA-Z]+$/", $data['lname']) || strlen($data['lname']) < 3){
-            //     $data['lname_err'] = "A valid last name is required";
-            // }
-
+            
         }else{
-            // $data = [
-            //     'pic' => '',
-            //     'fname' => '',
-            //     'lname' => '',
-            //     'curr_pwd' => '',
-            //     'new_pwd' => '',
-            //     'confirm_pwd' => '',
-            //     'fname_err' => '',
-            //     'lname_err' => '',
-            //     'curr_pwd_err' => '',
-            //     'new_pwd_err' => '',
-            //     'confirm_pwd_err' => ''
-            // ];
-
-            // $data['details'] = $this->profileModel->getPassengerDetails($_SESSION['user_id']);
-
-            // $this->view('passenger/edit_profile', $data);
+            $this->view('passenger/edit_profile');
         }
-    }
+    }*/
 }
