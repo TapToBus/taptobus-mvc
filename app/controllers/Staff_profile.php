3b517be11add641dac8dc359a48a42d3ab8e4b5f
Dasuni Dewani <dasunidewani1998@gmail.com>
2023-05-08 00:48:43 +0530

    coplete reject count in profile

--- app/controllers/Staff_profile.php
+++ app/controllers/Staff_profile.php
@@ -13,20 +13,30 @@
 
         public function viewprofile(){
             $staff_no = $_SESSION['user_id'];
-            $datacounts = [
+            // get total no of accepted requests
+            $Acceptdatacounts = [
                 'acceptOwners' => $this->profileModel->acceptedOwnerReq(),
                 'acceptConductor' => $this->profileModel->acceptedConductorReq(),
                 'acceptDriver' => $this->profileModel->acceptedDriverReq(),
                 'acceptBus' => $this->profileModel->acceptedBusReq()
             ];
 
-            $sum = array_sum($datacounts);
-            // print_r($sum);
-            // die();
+            $Acceptsum = array_sum($Acceptdatacounts);
+           
+            // get the total no of rejected requests
+            $Rejectdatacounts = [
+                'rejectOwners' => $this->profileModel->rejectedOwnerReq(),
+                'rejectConductor' => $this->profileModel->rejectedConductorReq(),
+                'rejecttDriver' => $this->profileModel->rejectedDriverReq(),
+                'rejecttBus' => $this->profileModel->rejectedBusReq()
+            ];
+            $Rejectsum = array_sum($Rejectdatacounts);
 
+            // passes staff info, request accept and reject counts.
             $data = [
                 'profile' => $this->profileModel->getStaffdetails($staff_no),
-                'TotalAccept' => $sum               
+                'TotalAccept' => $Acceptsum,
+                'TotalReject' => $Rejectsum            
             ];
             $this->view('staff/staff_profile',$data);
         }
--- app/models/M_staff_profile.php
+++ app/models/M_staff_profile.php
@@ -25,19 +25,45 @@ class M_staff_profile{
     public function acceptedConductorReq(){
         $this->db->query("SELECT COUNT(request_id) as conductorcount FROM conductor_request where status = 'active'");
         $result = $this->db->single();
-        return $result->conductorcount;   // If we pass $result object alone it will give an error. we must convert it into an string
+        return $result->conductorcount;   
 
     }
     public function acceptedDriverReq(){
         $this->db->query("SELECT COUNT(request_id) as drivercount FROM driver_request where status = 'active'");
         $result = $this->db->single();
-        return $result->drivercount;   // If we pass $result object alone it will give an error. we must convert it into an string
+        return $result->drivercount;   
 
     }
     public function acceptedBusReq(){
         $this->db->query("SELECT COUNT(request_id) as buscount FROM bus_request where status = 'active'");
         $result = $this->db->single();
-        return $result->buscount;   // If we pass $result object alone it will give an error. we must convert it into an string
+        return $result->buscount;   
+
+    }
+
+      // rejected counts
+      public function rejectedOwnerReq(){
+        $this->db->query("SELECT COUNT(request_id) as ownercount FROM owner_request where status = 'rejected'");
+        $result = $this->db->single();
+        return $result->ownercount;   
+
+    }
+    public function rejectedConductorReq(){
+        $this->db->query("SELECT COUNT(request_id) as conductorcount FROM conductor_request where status = 'rejected'");
+        $result = $this->db->single();
+        return $result->conductorcount;   
+
+    }
+    public function rejectedDriverReq(){
+        $this->db->query("SELECT COUNT(request_id) as drivercount FROM driver_request where status = 'rejected'");
+        $result = $this->db->single();
+        return $result->drivercount;   
+
+    }
+    public function rejectedBusReq(){
+        $this->db->query("SELECT COUNT(request_id) as buscount FROM bus_request where status = 'rejected'");
+        $result = $this->db->single();
+        return $result->buscount;   
 
     }
 
