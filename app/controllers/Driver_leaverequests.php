d453398e5c41785696e1356433ad099e7da71922
Nirodha <2020cs064@stu.ucsc.cmb.ac.lk>
2023-05-08 06:45:38 +0530

    commits

---
+++ app/controllers/Driver_leaverequests.php
@@ -0,0 +1,57 @@
+<?php
+
+class Driver_leaverequests extends Controller
+{
+
+    private $requestModel;
+
+    public function __construct()
+    {
+        if (!isLoggedIn()) {
+            direct('users/login');
+        }
+
+        $this->requestModel = $this->model('m_driver_leaverequests');
+    }
+
+    public function add_leaverequests()
+    {
+
+        $new =  $this->requestModel->get_user_details();
+        $user_ntc = $new->ntcNo;
+        $data1 =  $this->requestModel->view_leaverequests($user_ntc);
+    
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+            $data = [
+                'date_from' => $_POST['date_from'],
+                'date_to' => $_POST['date_to'],
+                'reason' => $_POST['reason'],
+
+            ];
+
+
+            if ($this->requestModel->add_leaverequests($data,$new)) {
+                direct('driver_leaverequests/add_leaverequests');
+            } else {
+                die('Sorry! Something went wrong');
+            }
+        } 
+        
+        else {
+            
+            $data = [
+                'date_from' => '',
+                'date_to' => '',
+                'reason' => '',
+
+            ];
+
+            // load the view
+            $this->view('driver/view_leaverequests', $data, $data1);
+        }
+    }
+
+  
+}
